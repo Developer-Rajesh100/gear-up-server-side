ad25960e9f995e805df4a6b561d60dd1268bdaff
Developer-Rajesh <rajeshhabra100@gmail.com>
2022-05-09 19:24:25 +0530

    Delete api complete

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const { query } = require("express");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 const app = express();
@@ -34,6 +35,53 @@ async function run() {
       const query = { _id: ObjectId(id) };
       const product = await productCollection.findOne(query);
       res.send(product);
+    });
+    //POST
+    app.post("/product", async (req, res) => {
+      const addQuantity = req.body;
+      const result = await productCollection.insertOne(addQuantity);
+      res.send(result);
+    });
+    //DELETE
+    app.delete("/product/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await productCollection.deleteOne(query);
+      res.send(result);
+    });
+    //DELEVER
+    app.put("/product/:id", async (req, res) => {
+      const deleverProduct = req.body.newQuentity;
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const product = await productCollection.findOne(query);
+      const option = { upsert: true };
+      const updatePro = {
+        $set: {
+          ...product,
+          quentity: deleverProduct,
+        },
+      };
+      const result = await productCollection.updateOne(
+        query,
+        updatePro,
+        Option
+      );
+      res.send(result);
+    });
+    // ADD QUENTITY
+    app.put("/product/:id", async (req, tes) => {
+      const deleverProduct = req.body.newQuentity;
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const product = await productCollection.findOne(query);
+      const option = { upsert: true };
+      const updatePro = {
+        $set: {
+          ...product,
+          quantity: deleverProduct,
+        },
+      };
     });
   } finally {
   }
